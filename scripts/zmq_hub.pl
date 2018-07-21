a74b5343371dc3fab2bc01381454d8aa5cd1b911
eoswatch <eoswatch@eoswatch.info>
2018-07-21 14:05:10 -0400

    added scripts/zmq_hub.pl

---
+++ scripts/zmq_hub.pl
@@ -0,0 +1,76 @@
+use strict;
+use warnings;
+use ZMQ::LibZMQ3;
+use ZMQ::Constants ':all';
+use Getopt::Long;
+
+
+
+my $ep_pull;
+my @ep_push;
+my @ep_pub;
+
+
+my $ok = GetOptions
+    ('pull=s'    => \$ep_pull,
+     'push=s'    => \@ep_push,
+     'pub=s'     => \@ep_pub);
+
+if( not $ok or scalar(@ARGV) > 0 or not $ep_pull or
+    scalar(@ep_push)+scalar(@ep_pub) == 0 )
+{
+    print STDERR "Usage: $0 --pull=ENDPOINT [options...]\n",
+    "The utility connects to ZMQ PUSH socket and \n",
+    "copies the messages to PUSH or PUB sockets.\n",
+    "At least one push or pub socket needs to be defined.\n",
+    "Multiple push and pub sockets can be defined.\n",
+    "Options:\n",
+    "  --pull=ENDPOINT\n",
+    "  --push=ENDPOINT\n",
+    "  --pub=ENDPOINT\n";
+    exit 1;
+}
+
+
+my $ctxt = zmq_init;
+
+
+my $s_pull = zmq_socket( $ctxt, ZMQ_PULL );
+my $rv = zmq_connect( $s_pull, $ep_pull );
+die($!) if $rv;
+
+my @s_push;
+my @s_pub;
+
+foreach my $ep (@ep_push)
+{
+    my $s =  zmq_socket( $ctxt, ZMQ_PUSH );
+    $rv = zmq_bind( $s, $ep );
+    die($!) if $rv;
+    push(@s_push, $s);
+}
+
+foreach my $ep (@ep_pub)
+{
+    my $s = zmq_socket( $ctxt, ZMQ_PUB );
+    $rv = zmq_bind( $s, $ep );
+    die($!) if $rv;
+    push(@s_pub, $s);
+}
+    
+
+
+my $inmsg = zmq_msg_init();
+while( zmq_msg_recv($inmsg, $s_pull) != -1 )
+{
+    my $indata = zmq_msg_data( $inmsg );
+    foreach my $s (@s_push, @s_pub)
+    {
+        my $outmsg = zmq_msg_init_data($indata);
+        zmq_msg_send($outmsg, $s);
+    }
+}
+
+
+print STDERR "The stream ended\n";
+

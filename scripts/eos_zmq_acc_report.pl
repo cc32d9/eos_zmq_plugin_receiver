3bbbc8b2a0d474a2c595b578dbdb973a9c96039d
eoswatch <eoswatch@eoswatch.info>
2018-07-24 09:27:38 -0400

    eos_zmq_acc_report.pl: improved reporting

--- scripts/eos_zmq_acc_report.pl
+++ scripts/eos_zmq_acc_report.pl
@@ -4,7 +4,7 @@ use JSON;
 use Getopt::Long;
 use DBI;
 use Excel::Writer::XLSX; 
-
+use Excel::Writer::XLSX::Utility;
 
 my $account;
 my $xlsx_out;
@@ -93,14 +93,19 @@ my %col_money =
 my %col_integer =
     ('ram_quota' => 1,
      'ram_usage' => 1);
-     
+
+my %col_datetime =
+    ('block_time' => 1);
+
+my ($first_ts, $last_ts);
+
 my $dbh = DBI->connect($dsn, $db_user, $db_password,
                        {'RaiseError' => 1, AutoCommit => 0,
                         mysql_server_prepare => 1});
 die($DBI::errstr) unless $dbh;
 
 my $workbook = Excel::Writer::XLSX->new($xlsx_out) or die($!);
-my $worksheet = $workbook->add_worksheet($start_date . ' ' . $months . ' months');
+my $worksheet = $workbook->add_worksheet($account . ' ' . $start_date . ' ' . $months . 'M');
 
 my $c_tblheader = $workbook->set_custom_color(40, '#003366');
 
@@ -111,7 +116,11 @@ my $f_tblheader = $workbook->add_format
       bg_color => $c_tblheader,
       color => 'white' ); 
 
+my $f_datetime = $workbook->add_format(num_format => 'yyyy-mm-dd hh:mm:ss');
 my $f_money = $workbook->add_format(num_format => '0.0000');
+my $f_bold = $workbook->add_format(bold => 1);
+my $f_money_bold = $workbook->add_format(bold => 1, num_format => '0.0000');
+
 
 my $col = 0;
 my $row = 0;
@@ -159,7 +168,14 @@ while( my $r = $sth->fetchrow_hashref('NAME_lc') )
     $final_stake{'net'} = $r->{'net_stake'};
     $final_ram{'quota'} = $r->{'ram_quota'};
     $final_ram{'usage'} = $r->{'ram_usage'};
-        
+
+    if( not defined($first_ts) )
+    {
+        $first_ts = $r->{'block_time'};
+    }
+
+    $last_ts = $r->{'block_time'};
+    
     my $action = $json->decode($r->{'jsdata'});
     my $atrace = $action->{'action_trace'};
 
@@ -233,10 +249,16 @@ while( my $r = $sth->fetchrow_hashref('NAME_lc') )
     
     foreach my $colname (@columns)
     {
-        if( $col_money{$colname} )
+        if( $col_datetime{$colname} )
+        {
+            my $val = $r->{$colname};
+            $val =~ s/\s/T/;
+            $worksheet->write_date_time($row, $col, $val, $f_datetime);
+        }
+        elsif( $col_money{$colname} )
         {
             $worksheet->write_number($row, $col, $r->{$colname}, $f_money);
-        }
+        }        
         elsif( $col_integer{$colname} )
         {
             $worksheet->write_number($row, $col, $r->{$colname});
@@ -252,36 +274,63 @@ while( my $r = $sth->fetchrow_hashref('NAME_lc') )
 $dbh->disconnect();
 
 
-$worksheet = $workbook->add_worksheet('final balance');
+$first_ts =~ s/\s/T/;
+$last_ts =~ s/\s/T/;
+
+$worksheet = $workbook->add_worksheet($account . ' final balance');
 
 $col = 0;
 $row = 0;
 
+$worksheet->set_column($col, $col, 40);
+$col++;
+$worksheet->set_column($col, $col, 40);
+$col++;
+$worksheet->set_column($col, $col, 40);
+$col++;
 $worksheet->set_column($col, $col, 20);
+$col++;
+
+
+$col = 0;
+$worksheet->write($row, $col, 'Account:');
+$col++;
+$worksheet->write($row, $col, $account, $f_bold);
+$col = 0;
+$row++;
+$worksheet->write($row, $col, 'First transaction:');
+$col++;
+$worksheet->write_date_time($row, $col, $first_ts, $f_datetime);
+$col = 0;
+$row++;
+$worksheet->write($row, $col, 'Last transaction:');
+$col++;
+$worksheet->write_date_time($row, $col, $last_ts, $f_datetime);
+
+$col = 0;
+$row+=2;
+$worksheet->write($row, $col, 'EOS assets:', $f_bold);
+$col = 0;
+$row++;
 $worksheet->write($row, $col, 'issuer', $f_tblheader);
 $col++;
-
-$worksheet->set_column($col, $col, 40);
 $worksheet->write($row, $col, 'category', $f_tblheader);
 $col++;
-
-$worksheet->set_column($col, $col, 40);
 $worksheet->write($row, $col, 'asset', $f_tblheader);
 $col++;
-
-$worksheet->set_column($col, $col, 20);
 $worksheet->write($row, $col, 'amount', $f_tblheader);
 $col++;
 
 $col = 0;
 $row++;
+my $r_sum_a = $row;
 $worksheet->write_string($row, $col, 'eosio.token');
 $col++;
 $col++;
 $worksheet->write_string($row, $col, 'EOS');
 $col++;
 $worksheet->write_number($row, $col, $final_balance{'eosio.token'}{'EOS'},  $f_money);
-
+my $total_eos = $final_balance{'eosio.token'}{'EOS'};
 
 $col = 0;
 $row++;
@@ -292,10 +341,11 @@ $col++;
 $worksheet->write_string($row, $col, 'EOS');
 $col++;
 $worksheet->write_number($row, $col, $final_stake{'cpu'},  $f_money);
-
+$total_eos += $final_stake{'cpu'};
 
 $col = 0;
 $row++;
+my $r_sum_b = $row;
 $worksheet->write_string($row, $col, 'eosio');
 $col++;
 $worksheet->write_string($row, $col, 'net_stake');
@@ -303,6 +353,32 @@ $col++;
 $worksheet->write_string($row, $col, 'EOS');
 $col++;
 $worksheet->write_number($row, $col, $final_stake{'net'},  $f_money);
+$total_eos += $final_stake{'net'};
+
+$col = 1;
+$row++;
+$worksheet->write_string($row, $col, 'TOTAL', $f_bold);
+$col++;
+$worksheet->write_string($row, $col, 'EOS');
+$col++;
+$worksheet->write_formula
+    ($row, $col,
+     '=SUM(' . xl_range($r_sum_a, $r_sum_b, $col, $col) . ')',
+     $f_money_bold, $total_eos);
+
+$col = 0;
+$row+=2;
+$worksheet->write($row, $col, 'RAM:', $f_bold);
+$col = 0;
+$row++;
+$worksheet->write($row, $col, 'issuer', $f_tblheader);
+$col++;
+$worksheet->write($row, $col, 'category', $f_tblheader);
+$col++;
+$worksheet->write($row, $col, 'asset', $f_tblheader);
+$col++;
+$worksheet->write($row, $col, 'amount', $f_tblheader);
+$col++;
 
 
 $col = 0;
@@ -313,7 +389,7 @@ $worksheet->write_string($row, $col, 'ram_quota');
 $col++;
 $worksheet->write_string($row, $col, 'bytes');
 $col++;
-$worksheet->write_number($row, $col, $final_ram{'quota'});
+$worksheet->write_number($row, $col, $final_ram{'quota'}, $f_bold);
 
 
 $col = 0;
@@ -327,7 +403,21 @@ $col++;
 $worksheet->write_number($row, $col, $final_ram{'usage'});
 
 
+$col = 0;
 $row+=2;
+$worksheet->write($row, $col, 'Tokens:', $f_bold);
+$col = 0;
+$row++;
+$worksheet->write($row, $col, 'issuer', $f_tblheader);
+$col++;
+$worksheet->write($row, $col, 'category', $f_tblheader);
+$col++;
+$worksheet->write($row, $col, 'asset', $f_tblheader);
+$col++;
+$worksheet->write($row, $col, 'amount', $f_tblheader);
+$col++;
+
+
 foreach my $issuer (sort keys %final_balance)
 {
     next if $issuer eq 'eosio.token';
